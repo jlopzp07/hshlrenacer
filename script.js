2a653fbeda62660e816d74a83eb037264305d91d
Stiven <donswae@gmail.com>
2024-05-24 11:39:18 -0500

    services CSS added and JS edited

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 document.getElementById('toggleServices').addEventListener('click', function() {
     const hiddenRows = document.querySelectorAll('.hidden-row');
     hiddenRows.forEach(row => {
-        row.style.display = (row.style.display === 'none' || row.style.display === '') ? 'grid' : 'none';
+        if (row.classList.contains('show')) {
+            row.classList.remove('show');
+            setTimeout(() => {
+                row.style.display = 'none';
+            }, 500); // Duración de la transición en ms
+        } else {
+            row.style.display = 'grid';
+            setTimeout(() => {
+                row.classList.add('show');
+            }, 10); // Pequeño retraso para permitir la actualización del DOM
+        }
     });
 
     // Cambiar el texto del botón según el estado de las filas
     this.textContent = (this.textContent === 'Conoce todos nuestros servicios') ? 'Mostrar menos servicios' : 'Conoce todos nuestros servicios';
+
+    // Si se están ocultando las filas, desplazar la vista hacia la sección de servicios
+    if (this.textContent === 'Conoce todos nuestros servicios') {
+        document.getElementById('services').scrollIntoView({ behavior: 'smooth' });
+    }
 });
